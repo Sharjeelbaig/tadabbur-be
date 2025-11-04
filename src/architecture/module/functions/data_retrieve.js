f7ca4b302e49ee8c35a70600c8c72022056b77a3
Sharjeel Baig <dr.sharjeel.6@gmail.com>
2025-11-04 14:55:41 +0500

    Refactor retrieveSurah function to use 'textIndopak' instead of 'textUthmani' for verse and word retrieval

--- src/architecture/module/functions/data_retrieve.js
+++ src/architecture/module/functions/data_retrieve.js
@@ -4,15 +4,15 @@ export async function retrieveSurah(surahId, translation_id) {
     const surah = await quranClient?.verses?.findByChapter(surahId, {
         words: true,
         translations: [translation_id],
-        wordFields: ['text_uthmani'],
-        per_page: 300, // big enough to cover the entire surah
-    page: 1,
+        wordFields: ['textIndopak'],
+        per_page: 300,
+        page: 1
     })
     const verses = surah?.map(data => {
     const translation = data?.translations?.map(translation => translation?.text)[0]
-    const words = data?.words?.map(words => words?.textUthmani) 
+    const words = data?.words?.map(words => words?.textIndopak) 
     const word_audios = data?.words?.map(words => 'https://audio.qurancdn.com/'+words?.audioUrl)?.slice(0, -1)
-    const verse = data?.words?.map(words => words?.textUthmani)?.join(' ')
+    const verse = data?.words?.map(words => words?.textIndopak)?.join(' ')
     const key = data?.verseKey
     return (
         {
@@ -28,25 +28,21 @@ export async function retrieveSurah(surahId, translation_id) {
 }
 
 export async function retrieveTafseer(surahId, tafsirId) {
-    const tafseer_data = await fetch(
-      `https://api.quran.com/api/v4/tafsirs/${tafsirId}/by_chapter/${surahId}?per_page=300&page=1`
-    )
+    const tafseer_data = await fetch(`https://api.quran.com/api/v4/tafsirs/${tafsirId}/by_chapter/${surahId}?per_page=300&page=1`)
     const tafseer = await tafseer_data.json()
     return tafseer;
 }
 
 export async function retrieveRecitation(chapterId, reciterId) {
-  const recitation = await quranClient.audio.findVerseRecitationsByChapter(chapterId, reciterId, {
-    per_page: 300,
-    page: 1
-  });
-
-  const audioFiles = recitation.audioFiles.map(verse => {
-    const verseKey = verse.verseKey;
-    const audioUrl = 'https://audio.qurancdn.com/' + verse.url;
-    return { verseKey, audioUrl };
-  });
-
-  return audioFiles;
+    const recitation = await quranClient?.audio?.findVerseRecitationsByChapter(chapterId, reciterId, {
+        per_page: 300,
+        page: 1
+    });
+    const audioFiles = recitation?.audioFiles?.map(verse => {
+        const verseKey = verse?.verseKey;
+        const audioUrl = 'https://audio.qurancdn.com/' + verse?.url;
+        return { verseKey, audioUrl };
+    })
+    return audioFiles;
 }
 
